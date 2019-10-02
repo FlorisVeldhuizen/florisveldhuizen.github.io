ff810a92781360444cea90035163e54121553fc3
Floris <flm.veldhuizen@gmail.com>
2019-10-02 17:31:52 +0200

    updated data storage, added all pages, fixed id issues

--- reviewthesis/scripts/usertest.js
+++ reviewthesis/scripts/usertest.js
@@ -10,11 +10,11 @@ function saveFile(data,filename){
 }
 
 function getUrlVars() {
-    let vars = {};
-    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
+  let vars = {};
+  let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+  });
+  return vars;
 }
 
 function setSessionID(){
@@ -30,20 +30,80 @@ function setSessionID(){
 
 function getSessionID(){
 	let vars = getUrlVars();
-	console.log(vars.id);
 	curID = vars.id;
 	return vars.id;
 }
 
-function fillData(){
-	curObj.id = curID;
-	curObj.q1 = "question 1 answer";
-	curObj.q2 = "question 2 answer";
+function getcheckedItem(inputname){
+	let rates = document.getElementsByName(inputname);
+	let rate_value;
+	for(let i = 0; i < rates.length; i++){
+	    if(rates[i].checked){
+	        rate_value = rates[i].value;
+	    }
+	}
+	return rate_value;
 }
 
-function nextPage(page){
+function setPageData(curpage,nextpage){
+	if(curpage === "introduction"){
+		let form = document.getElementById('introductionform');
+		if(form.checkValidity()){
+			updateData("gender",getcheckedItem('gender'));
+			updateData("age",form.age.value);
+			nextPage(nextpage);
+			return false;
+	  }
+	} else if (curpage === "review-mashup"){
+		let form = document.getElementById('coffeeform');
+		if(form.checkValidity()){
+			updateData("coffeemachine",getcheckedItem('coffeemachine'));
+			updateData("coffeemachinetext",form.coffeemachinetext.value);
+			nextPage(nextpage);
+			return false;
+		}
+	} else if (curpage === "review-credibility"){
+		let form = document.getElementById('credibilityform');
+		if(form.checkValidity()){
+			updateData("fullname",getcheckedItem('fullname'));
+			updateData("nickname",getcheckedItem('nickname'));
+			updateData("socialmedia",getcheckedItem('socialmedia'));
+			updateData("avatar",getcheckedItem('avatar'));
+			updateData("verified",getcheckedItem('verified'));
+			updateData("endorsed",getcheckedItem('endorsed'));
+			updateData("userfeedback",getcheckedItem('userfeedback'));
+			updateData("picture",getcheckedItem('picture'));
+			nextPage(nextpage);
+			return false;
+	  }
+	} else if (curpage === "review-itr"){
+		let form = document.getElementById('itrform');
+		if(form.checkValidity()){
+			updateData("prompt",getcheckedItem('prompt'));
+			updateData("prompttext",form.prompttext.value);
+			updateData("discount",getcheckedItem('discount'));
+			updateData("discounttext",form.discounttext.value);
+			updateData("credits",getcheckedItem('credits'));
+			updateData("creditstext",form.creditstext.value);
+			updateData("doubleblind",getcheckedItem('doubleblind'));
+			updateData("doubleblindtext",form.doubleblindtext.value);
+			nextPage(nextpage);
+			return false;
+		}
+  } else if (curpage === "review-itrreflection"){
+		let form = document.getElementById('itrreflectionform');
+		if(form.checkValidity()){
+			updateData("reflection",getcheckedItem('reflection'));
+			updateData("reflectiontext",form.reflectiontext.value);
+			nextPage(nextpage);
+			return false;
+	  }
+	}
+}
+
+function nextPage(nextpage){
 	const path = window.location.href;
-	const url = new URL(page,path);
+	const url = new URL(nextpage,path);
 	url.searchParams.set('id', curID);
 	window.location.href = url;
 }
@@ -57,17 +117,16 @@ function updateData(key,value){
 	localStorage.setItem(sessionID,strtempObj);
 }
 
-// fillData();
 function init(){
-	setSessionID();
-	updateData("oi","test");
-	updateData("yo","test");
+	if(getSessionID()===undefined||getSessionID()===null){
+		console.log('no session ID has been set, setting session ID...');
+		setSessionID();
+		console.log('session ID has been set to ' + curID + '.')
+	}
 }
 
 function setPage(){
 	getSessionID();
-	updateData("oi","test");
-	updateData("yo","test");
 }
 
 
