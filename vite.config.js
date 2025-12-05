fe5f30de390717303971d10e799ecd6cf99686de
FlorisVeldhuizen <flm.veldhuizen@gmail.com>
2025-12-05 14:15:43 +0100

    Fix Phaser asset loading by disabling inline data URIs

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
   build: {
     emptyOutDir: true,
     outDir: path.join(__dirname, "dist"),
+    assetsInlineLimit: 0,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
